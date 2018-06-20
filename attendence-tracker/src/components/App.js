5add5fa8dd29192b9956f74e768b98d1cac25cf9
Tiffany DeVine <tiffany.devine@icloud.com>
2018-06-20 09:08:51 -0700

    Created App component with StudentManager component being rendered

---
+++ attendence-tracker/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import StudentManager from "./StudentManager";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <StudentManager />
+      </div>
+    );
+  }
+}
+
+export default App;
