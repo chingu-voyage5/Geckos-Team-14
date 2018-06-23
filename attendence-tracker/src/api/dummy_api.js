6c0fe485ffbad643f206382d736e8b225b226b12
Regina Mc Guire <rmcguire@antelecom.net>
2018-06-23 14:09:06 -0700

    Add dummy_api file

---
+++ attendence-tracker/src/api/dummy_api.js
@@ -0,0 +1,35 @@
+module.exports = {
+  data: {
+    students: [
+      {
+        _id: 500,
+        firstName: "Amy",
+        lastName: "Begoud",
+        classes: ['English'],
+        attendance: []
+      },
+      {
+        _id: 504,
+        firstName: "Davis",
+        lastName: "Yaton",
+        classes: ['English', 'Spanish'],
+        attendance: []
+      },
+      {
+        _id: 503,
+        firstName: "Peter",
+        lastName: "Nichols",
+        classes: ['Spanish'],
+        attendance: []
+      },
+      {
+        _id: 507,
+        firstName: "Jane",
+        lastName: "Easton",
+        classes: ['French'],
+        attendance: []
+      }
+    ]
+  }
+};
+
--- attendence-tracker/src/components/App.js
+++ attendence-tracker/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import API from '/.api/dummy_api';
 import StudentManager from "./StudentManager";
 
 class App extends Component {
