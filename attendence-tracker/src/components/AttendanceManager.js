1d3401b8565faf8a42a2b101f40dc75c55e3b8c8
Regina Mc Guire <rmcguire@antelecom.net>
2018-07-18 10:35:00 -0700

    Add course student list to choose course form

--- attendence-tracker/src/components/AttendanceManager.js
+++ attendence-tracker/src/components/AttendanceManager.js
@@ -7,33 +7,60 @@ export default class AttendanceManager extends Component {
     this.state = {
       courses: data.courses,
       students: data.students,
-      value: 'No course selected'
+      value: 'No course selected',
+      chosenCourse: '',
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
+  };
   handleChange(e) {
     this.setState({ value: e.target.value });
-  }
+  };
 
   handleSubmit(e) {
-    alert('Your selected course is: ' + this.state.value);
+    this.setState({ chosenCourse: this.state.value })
     e.preventDefault();
-  }
-  render() {
-    const renderCourseList = () => {
-      let courseList = [];
-      for (var key in this.state.courses) {
-        courseList.push(this.state.courses[key].courseName);
-      }
-      console.log(courseList);
-      return courseList.map(item => (
-        <option key={item} value={item} >
-          {item}
-        </option>
-      ));
+  };
+  createCourseList = () => {
+    let courseList = [];
+    for (var label in this.state.courses) {
+      courseList.push(this.state.courses[label].courseName);
     }
+    return courseList;
+  };
+  createStudentList = () => {
+    let studentList = [];
+    for (var key in this.state.students) {
+      studentList.push(this.state.students[key].firstName + ' ' + this.state.students[key].lastName);
+    }
+    return studentList;
+  };
+
+  renderList = (list) => {
+    let renderedList = list.map(item => (
+      <option key={item} value={item} >
+        {item}
+      </option>
+    ));
+    return renderedList;
+  };
+  renderStudentList = (studentList) => {
+
+    if (this.state.chosenCourse === '') {
+      return <div><p>No Course Selected</p></div>
+    } else {
+
+      return <div><p>Course selected: {this.state.chosenCourse}</p>
+        <ul>
+          <p>{studentList}</p>
+        </ul>
+      </div>
+    }
+
+  };
+  render() {
+    let currentCourseList = this.renderList(this.createCourseList());
+    let currentStudentList = this.renderStudentList(this.createStudentList());
     return (
       <div>
         <h1>Take Attendance</h1>
@@ -41,11 +68,13 @@ export default class AttendanceManager extends Component {
           <label>
             <h3>Select a Course: </h3>
             <select value={this.state.value} onChange={this.handleChange}>
-              {renderCourseList()}
+              <option value="">Please select a course: </option>
+              {currentCourseList}
             </select>
           </label>
           <input type="submit" value="Submit" />
         </form>
+        {currentStudentList}
       </div >
     )
   }
